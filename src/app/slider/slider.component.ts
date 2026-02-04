f88d0f554fadca6a70e09a5b638cf6f708f6486c
CamaleonCuliao <alejandrogomezguillen9@gmail.com>
2026-02-04 21:09:03 +0100

    slider añadido, dar funcionalidad

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { url } from 'inspector';
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css'
 })
 export class SliderComponent {
+  imagenes = [
+    { url: "assets/img/museos/arqAlicante.jpg", titulo: "Museo de arqueologia de Alicante" },
+    { url: "assets/img/museos/arqMurcia.jpg", titulo: "Museo de arqueologia de Murcia" },
+    { url: "assets/img/museos/artAlicante.jpg", titulo: "Museo de bellas arte de Alicante" },
+    { url: "assets/img/museos/artMurcia.jpg", titulo: "Museo de bellas arte de Murcia" },
+  ]
 
+  index = 0;
+
+  siguiente() {
+    this.index = (this.index + 1) % this.imagenes.length;
+  }
+
+  anterior() {
+    this.index = (this.index - 1 + this.imagenes.length) % this.imagenes.length;
+  }
 }
