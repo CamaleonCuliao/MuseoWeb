034b5877de47ca446ec34eec414322c6b9c92fea
CamaleonCuliao <alejandrogomezguillen9@gmail.com>
2026-02-17 17:38:16 +0100

    preparacion diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
-import { NgForOf } from "@angular/common";
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { SliderComponent } from './slider/slider.component';
-import { CajaComponent } from './caja/caja.component';
 //import { SwiperModule } from '../../node_modules/swiper/swiper';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, SliderComponent, CajaComponent, NgForOf, /*SwiperModule*/],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'web-museo';
 
-  cantidad = [
-    "cosa", "cosa", "cosa", "cosa"
-  ];
 }
